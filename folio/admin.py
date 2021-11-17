f1b23e77c6144a2f0dd069a7a695ff5b21275ab6
Philippe Lemaire <philippe.lemaire@protonmail.com>
2021-11-17 17:46:30 +0100

    registered folio in admin

--- folio/admin.py
+++ folio/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
+from .models import Project, Tag
+from markdownx.admin import MarkdownxModelAdmin
 
 # Register your models here.
+
+
+class ProjectAdmin(MarkdownxModelAdmin):
+    list_display = [
+        "title",
+        "is_published",
+        "pub_date",
+    ]
+    list_filter = ["pub_date"]
+
+
+admin.site.register(Tag)
+admin.site.register(Project, ProjectAdmin)
