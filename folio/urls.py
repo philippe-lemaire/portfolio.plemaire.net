23a802af7f133ed9742c02d07387b2642b1ebb9b
Philippe Lemaire <philippe.lemaire@protonmail.com>
2021-11-17 15:51:40 +0100

    basic templates ok

--- folio/urls.py
+++ folio/urls.py
@@ -1,5 +1,10 @@
 from django.urls import path
 from . import views
+from django.views.generic import TemplateView
+
 
 app_name = "folio"
-urlpatterns = []
+urlpatterns = [
+    path("", TemplateView.as_view(template_name="folio/index.html"), name="index"),
+    path("projects/", views.ProjectListView.as_view(), name="projects"),
+]
