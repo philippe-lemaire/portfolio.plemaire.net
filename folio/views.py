5402ffb2158ddb670f80593a3b1e50b8122c084d
Philippe Lemaire <philippe.lemaire@protonmail.com>
2021-11-25 13:21:07 +0100

    fix to display only published project

--- folio/views.py
+++ folio/views.py
@@ -8,6 +8,10 @@ from .models import Project, Tag
 class ProjectListView(ListView):
     model = Project
 
+    def get_queryset(self):
+        queryset = Project.objects.filter(published=True)
+        return queryset
+
 
 class ProjectDetailView(DetailView):
     model = Project
